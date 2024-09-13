b55c3fef5a1a3416b9f516f6318784066b0281a9
oodool <bontangrt2@gmail.com>
2024-09-13 11:16:06 +0700

    pertama
    1

---
+++ Assets/Mouse.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mouse : MonoBehaviour
+{
+    public GameObject player;
+    private Vector2 mouse;
+    public float sens;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouse = UnityEngine.InputSystem.Mouse.current.position.ReadValue();
+        var worldPosition = Camera.main.ScreenToWorldPoint(mouse);
+        transform.position = Vector2.Lerp(player.transform.position, worldPosition, sens);
+    }
+}
